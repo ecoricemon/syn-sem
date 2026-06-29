c05689030305892a549e4a3794db516c62eccf28
ecoricemon <ecoricemon@gmail.com>
2026-06-29 09:24:10 +0900

    fmt

--- crates/syn-sem-infer/src/logic.rs
+++ crates/syn-sem-infer/src/logic.rs
@@ -5,13 +5,13 @@
 //! symbols, and small term utilities.
 
 mod atom;
-mod visit;
 mod equality;
 pub(crate) mod symbol;
+mod visit;
 
 pub(crate) use atom::{
     atom, def_id, expr_id, type_id, type_id_from_term, type_subject, CreateTerm, LogicAtom,
     LogicClause, LogicTerm,
 };
-pub(crate) use visit::visit_left_var;
 pub(crate) use equality::{same_type, same_type_rules, type_equal_clause, SameTypeRules};
+pub(crate) use visit::visit_left_var;
